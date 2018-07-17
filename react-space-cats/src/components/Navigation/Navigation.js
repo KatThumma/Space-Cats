8443b0bd208dc18ae3e1213b5ae684954a58e128
unknown <billy.vonne0930@gmail.com>
2018-07-16 21:02:53 -0400

    adding navbar, blank css for each component

--- react-space-cats/src/components/Navigation/Navigation.js
+++ react-space-cats/src/components/Navigation/Navigation.js
@@ -1,15 +1,37 @@
 import React from "react";
-
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { NavItem } from 'react-bootstrap';
 
 
 const Navigation = () => {
 
     return (
       <div>
-        <NavLink to="/">Homebase</NavLink>
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/Game1">Game</NavLink>
+        <Navbar>
+          <Navbar.Header>
+
+            <Navbar.Brand>
+              <NavLink to="/">Homebase</NavLink>
+            </Navbar.Brand>
+
+          </Navbar.Header>
+              <Nav>
+                <NavItem>
+                <NavLink eventKey={1} to="/login">
+                  Login
+                </NavLink>
+                </NavItem>
+
+                <NavItem>
+                <NavLink eventKey={2} to="/Game1">
+                  Game
+                </NavLink>
+                </NavItem>
+
+              </Nav>
+          </Navbar>;
       </div>
     );
   };
